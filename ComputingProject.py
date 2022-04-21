7208de2c903b0d1418243d5ac1ff2942a2979811
Caileigh Lavigne <cblavigne01@gmail.com>
2022-04-21 12:34:42 -0500

    Added and fixed code

--- ComputingProject.py
+++ ComputingProject.py
@@ -1,5 +1,14 @@
-print("Enter total amount of ___ added")
+import serial
+ser = serial.Serial('/dev/tty.usbserial-1410')
+
+print("Enter total amount of titrant added")
 
 # inputting the total volume
 volume = input("Volume:")
-print(f'Total volume: {volume}')
+print("Volume: {0} mL".format(volume))
+
+while volume != ""
+  ser.write(b'R\r')
+  float(ser.read(8))
+  # add csv file stuff
+  volume = input("Volume:")
