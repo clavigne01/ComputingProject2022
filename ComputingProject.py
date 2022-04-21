cdfcf454f0ccdce1a1595177bc2334a833d4f101
Dereck Jernigan <dereckjernigan@okbu.edu>
2022-04-21 13:03:22 -0500

    Added 'csv file stuff'

--- ComputingProject.py
+++ ComputingProject.py
@@ -10,5 +10,14 @@ print("Volume: {0} mL".format(volume))
 while volume != ""
   ser.write(b'R\r')
   float(ser.read(8))
-  # add csv file stuff
+
+```{r}
+import csv
+with open('_____', 'w', newline='') as csvfile:
+    _____ = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+   ______.writerow(['_____'] * 5 + ['_____'])
+    _____.writerow(['_____', '______', '_____'])
+```
+
   volume = input("Volume:")
