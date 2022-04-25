92ed7356bda3da89bc7dbc0310c0068ab0054acc
Dereck Jernigan <dereckjernigan@okbu.edu>
2022-04-25 08:50:29 -0500

    fixed csv stuff

--- ComputingProject.py
+++ ComputingProject.py
@@ -1,4 +1,5 @@
 import serial
+import csv
 ser = serial.Serial('/dev/tty.usbserial-1410')
 
 print("Enter total amount of titrant added")
@@ -7,17 +8,18 @@ print("Enter total amount of titrant added")
 volume = input("Volume:")
 print("Volume: {0} mL".format(volume))
 
-while volume != ""
-  ser.write(b'R\r')
-  float(ser.read(8))
-
-```{r}
-import csv
-with open('_____', 'w', newline='') as csvfile:
-    _____ = csv.writer(csvfile, delimiter=' ',
+with open('ComputingProject.csv', 'w', newline='') as csvfile:
+  compwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-   ______.writerow(['_____'] * 5 + ['_____'])
-    _____.writerow(['_____', '______', '_____'])
-```
+  compwriter.writerow(['Volume'], ['pH'])
+  
 
-  volume = input("Volume:")
+  while volume != ""
+    ser.write(b'R\r')
+    pH = float(ser.read(8))
+  
+
+ 
+    compwriter.writerow([volume, pH])
+
+    volume = input("Volume:")
